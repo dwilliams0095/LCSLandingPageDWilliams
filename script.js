276b09bbb43b343c51f4b8cfc79c95e05f03ea7b
dwilliams0095 <65625025+dwilliams0095@users.noreply.github.com>
2020-06-05 17:08:07 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var myIndex = 0;
+carousel();
+carousel2();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("slide");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(carousel, 3000);
+}
+
+function carousel2() {
+  var i;
+  var x = document.getElementsByClassName("slide2");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(carousel2, 4000);
+}
+
